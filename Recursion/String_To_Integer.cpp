db5adad0fe4e87461acad20b2868c9993c4aba61
Deepak Mehra <mehradeepak0608@gmail.com>
2020-09-11 23:53:34 +0530

    added new programs

--- Recursion/String_To_Integer.cpp
+++ Recursion/String_To_Integer.cpp
@@ -1,1 +1,21 @@
+#include<string.h>
+int helper(char input[], int last)
+{
+    if(last==0)
+    {
+        return input[last]-'0';
+    }
 
+    int smallAns = helper(input,last -1);
+    int a = input[last]-'0';
+
+    return smallAns*10 + a;
+}
+int stringToNumber(char input[])
+{
+    int len = strlen(input);
+
+    int ans = helper(input, len-1);
+
+    return ans;
+}
