c354d7a1f6898210d682bc58b5fba50b8a192c1e
Deepak Mehra <mehradeepak0608@gmail.com>
2020-09-27 14:26:16 +0530

    added print Tree

---
+++ Trees/Print_Tree.cpp
@@ -0,0 +1,14 @@
+void printTree(TreeNode<int>* root)
+{
+    cout<<root->data<<":";
+
+    for(int i=0;i<root->children.size();i++)
+    {
+        cout<<root->children[i]->data<<",";
+    }
+    cout<<endl;
+    for(int i=0;i<root->children.size();i++)
+    {
+        printTree(root->children[i]);
+    }
+}
